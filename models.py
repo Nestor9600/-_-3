3ce7b07124c117f2b174ab3c38e913cc7780f40b
Nestor9600 <149958818+Nestor9600@users.noreply.github.com>
2024-04-27 14:44:47 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,35 @@
+from app import db, manager
+from flask_login import UserMixin
+import datetime
+#import requests
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128), nullable=False, unique=True)
+    psw = db.Column(db.Text, nullable=False)
+    opinions = db.relationship('Opinion', backref='author')
+
+    def __repr__(self):
+        return f'<Users {self.title}>'
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime,
+                     default=datetime.datetime.now)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #img_data = requests.get('http://127.0.0.1:8000').content
+    #with open('*.jpg', 'wb') as handler:
+        #handler.write(img_data)
+
+    def __repr__(self):
+        return f'<Opinions {self.title}>'
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
---
+++ routes.py
@@ -0,0 +1,162 @@
+from app import app, db, manager
+from app.models import Opinion, User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import render_template, redirect, url_for, request, flash, abort
+from flask_login import login_user, logout_user, login_required, current_user
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    opinions = Opinion.query.order_by(Opinion.date.desc()).all()
+    return render_template('index.html', opinions=opinions)
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+# Записи
+
+
+@app.route('/create', methods=['POST', 'GET'])
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        f = request.files['file']
+
+        author = current_user.id
+        opinion = Opinion(title=title, intro=intro, text=text, author_id=author)
+
+        try:
+            db.session.add(opinion)
+            name = str(author) + '.jpg'
+            out = open(name, "wb")
+            out.write(f.content)
+            out.close()
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Возникла ошибка при добавлении публикации'
+    else:
+        return render_template('create.html')
+
+
+@app.route('/<int:id>/')
+def detail(id):
+    opinion = Opinion.query.get(id)
+    return render_template('detail.html', opinion=opinion)
+
+
+# Изменение
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        f = request.files['file']
+
+        author = current_user.id
+        opinion = Opinion(title=title, intro=intro, text=text, author_id=author)
+
+        try:
+            db.session.add(opinion)
+            name = str(author) + '.jpg'
+            out = open(name, "wb")
+            out.write(f.content)
+            out.close()
+            opinion = db.session.get(Opinion, id)
+            db.session.delete(opinion)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Возникла ошибка при изменении объявления'
+    else:
+        opinion = db.session.get(Opinion, id)
+        title = opinion.title
+        intro = opinion.intro
+        text = opinion.text
+        author = current_user.id
+        return render_template('edit.html', title=title, intro=intro, text=text, author_id=author)
+
+
+# Удаление
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db.session()
+    news = db_sess.query(Opinion).filter(Opinion.id == id).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
+# Пользователь
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    psw = request.form.get('psw')
+
+    if login and psw:
+        user = User.query.filter_by(login=login).first()
+
+        if user and check_password_hash(user.psw, psw):
+            login_user(user)
+            return redirect(url_for('profile'))
+        else:
+            flash("Логин или пароль указаны неверно")
+    else:
+        flash('Заполните все поля')
+    return render_template('login.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    login = request.form.get('login')
+    psw = request.form.get('psw')
+    psw2 = request.form.get('psw2')
+
+    if request.method == 'POST':
+        if not (login or psw or psw2):
+            flash('Заполните все поля')
+        elif psw != psw2:
+            flash('Пароли не совпадают')
+        else:
+            hash_psw = generate_password_hash(psw)
+            new_user = User(login=login, psw=hash_psw)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login_page'))
+
+    return render_template('registration.html')
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@manager.unauthorized_handler
+def unauthorized():
+    flash('Вы не вошли в систему.')
+    return redirect(url_for('login_page'))
